d27831479477ec8637092e37c279061fea5191c6
boyroywax <jmsdevln@gmail.com>
2023-06-27 14:50:19 -0700

    clean branch

--- tests/tests.py
+++ tests/tests.py
@@ -1,13 +1,7 @@
 import unittest
 
-from .tests_super import TestSuper
-# from .tests_value import TestValue
-# from .tests_system import TestSystem
-# from .tests_unit import TestUnit
-# from .tests_type import TestType
-# from .tests_nonce import TestNonce
-# from .tests_units import TestUnits
-# from .tests_defaults import TestDefaults
+# from .tests_super import TestSuper
+
 
 
 
@@ -19,14 +13,8 @@ def main():
     test_loader = unittest.TestLoader()
 
     # Add the test cases to the test suite
-    test_suite.addTests(test_loader.loadTestsFromTestCase(TestSuper))
-    # test_suite.addTests(test_loader.loadTestsFromTestCase(TestValue))
-    # test_suite.addTests(test_loader.loadTestsFromTestCase(TestUnit))
-    # test_suite.addTests(test_loader.loadTestsFromTestCase(TestSystem))
-    # test_suite.addTests(test_loader.loadTestsFromTestCase(TestType))
-    # test_suite.addTests(test_loader.loadTestsFromTestCase(TestNonce))
-    # test_suite.addTests(test_loader.loadTestsFromTestCase(TestUnits))
-    # test_suite.addTests(test_loader.loadTestsFromTestCase(TestDefaults))
+    # test_suite.addTests(test_loader.loadTestsFromTestCase(TestSuper))
+
 
     # Run the test suite
     runner = unittest.TextTestRunner()
