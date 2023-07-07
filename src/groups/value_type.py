a544b09eab333680a94b9c559d0ed109f921d8c6
boyroywax <jmsdevln@gmail.com>
2023-07-07 10:46:46 -0700

    created new file for value_type_ref class

--- src/groups/value_type.py
+++ src/groups/value_type.py
@@ -4,51 +4,7 @@ from typing import Any, List, Optional, Tuple
 
 from .decorators import check_frozen
 from .frozen import Frozen, FrozenInterface
-
-
-
-class ValueTypeRefInterface(FrozenInterface):
-    """
-    The interface for the ValueTypeRef class.
-    """
-    _alias: str
-
-    @property
-    @abstractmethod
-    def alias(self) -> str:
-        """
-        The alias of the value type.
-        """
-        pass
-
-
-@dataclass(slots=True)
-class ValueTypeRef(ValueTypeRefInterface, Frozen):
-    """
-    The Value Type Ref class.
-    """
-    _alias: str = field(default_factory=str, repr=True)
-
-    def __init__(self, alias: str) -> None:
-        """
-        Initializes the ValueTypeRef class.
-        """
-        self._alias = alias
-
-    @property
-    def alias(self) -> str:
-        """
-        The alias of the value type.
-        """
-        return self._alias
-
-    @alias.setter
-    @check_frozen
-    def alias(self, alias: str) -> None:
-        """
-        Sets the alias of the value type.
-        """
-        self._alias = alias
+from .value_type_ref import ValueTypeRef, ValueTypeRefInterface
 
 
 @dataclass(slots=True)
