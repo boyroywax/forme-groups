ba6de298acd93c82daab997dbcf16278887b86bd
boyroywax <jmsdevln@gmail.com>
2023-09-25 15:55:40 -0700

    devj base

--- src/groups/base.py
+++ src/groups/base.py
@@ -1,9 +1,62 @@
-"""
-The base class for all group units.
 
-"""
+from attrs import define, field, validators
+from typing import List, Any, Optional, Dict
+
 
 class Base:
-    """
+    _base_value_type: Any = field(default=str, validator=validators.instance_of(type))
+
+    @define(frozen=True, slots=True)
+    class Value_:
+        _value: Any
+
+    @define(frozen=True, slots=True)
+    class Function_:
+        _object: 'Base.Value_'
+        _args: List['Base.Value_']
+
+    @define(frozen=True, slots=True)
+    class Container_:
+        _prefix: 'Base.Value_'
+        _suffix: 'Base.Value_'
+        _separator: 'Base.Value_'
+
+    @define(frozen=True, slots=True)
+    class Type_:
+        _aliases: List['Base.Value_']
+        _super_type: 'Base.Type_'
+        _is_container: bool
+        _container: Optional['Base.Container_']
+        _function: Optional['Base.Function_']
+
+        @define(frozen=True, slots=True)
+        class Reference_:
+            _alias: 'Base.Value_'
+
+        @property
+        def ref(self) -> 'Base.Type_.Reference_':
+            return self.Reference_(self._aliases[0])
+
+    @define(frozen=True, slots=True)
+    class Schema_:
+        _pattern: Dict['Base.Value_', 'Base.Type_.Reference_']
+
+    @define(frozen=True, slots=True)
+    class Unit_:
+        _value: 'Base.Value_'
+        _type_ref: 'Base.Type_.Reference_'
+
+    @define(frozen=True, slots=True)
+    class Type_Pool_:
+        _types: List['Base.Type_']
     
-    """
+    class Unit_Pool_:
+        _units: List['Base.Unit_']
+
+    class Group_:
+        _type_pool: 'Base.Type_Pool_'
+        _unit_pool: 'Base.Unit_Pool_'
+
+
+    
+
--- tests/tests_unit.py
+++ tests/tests_unit.py
@@ -10,8 +10,69 @@ from src.groups.base import Base
 
 class TestBase(unittest.TestCase):
     def setUp(self):
-        self.base_class = Base()
+        self.base = Base()
 
-    def test_base_class(self):
-        self.assertIsInstance(self.base_class, Base)
+    def test_value(self):
+        value = self.base.Value_(123)
+        self.assertEqual(value._value, 123)
 
+    def test_function(self):
+        value = self.base.Value_(123)
+        func = self.base.Function_(value, [value])
+        self.assertEqual(func._object, value)
+        self.assertEqual(func._args, [value])
+
+    def test_container(self):
+        prefix = self.base.Value_('prefix')
+        suffix = self.base.Value_('suffix')
+        sep = self.base.Value_('sep')
+        container = self.base.Container_(prefix, suffix, sep)
+        self.assertEqual(container._prefix, prefix)
+        self.assertEqual(container._suffix, suffix)
+        self.assertEqual(container._separator, sep)
+
+    def test_type(self):
+        aliases = [self.base.Value_('alias1'), self.base.Value_('alias2')]
+        super_type = self.base.Type_(aliases, None, False, None, None)
+        container = self.base.Container_(None, None, None)
+        func = self.base.Function_(None, [])
+        typ = self.base.Type_(aliases, super_type, True, container, func)
+        self.assertEqual(typ._aliases, aliases)
+        self.assertEqual(typ._super_type, super_type)
+        self.assertTrue(typ._is_container)
+        self.assertEqual(typ._container, container)
+        self.assertEqual(typ._function, func)
+
+    def test_schema(self):
+        unit1 = self.base.Unit_(self.base.Value_('value1'), self.base.Type_([], None, False, None, None))
+        unit2 = self.base.Unit_(self.base.Value_('value2'), self.base.Type_([], None, False, None, None))
+        pattern = {unit1: unit2}
+        schema = self.base.Schema_(pattern)
+        self.assertEqual(schema._pattern, pattern)
+
+    def test_unit(self):
+        value = self.base.Value_('value')
+        typ_ref = self.base.Type_.Reference_(self.base.Value_('alias'))
+        unit = self.base.Unit_(value, typ_ref)
+        self.assertEqual(unit._value, value)
+        self.assertEqual(unit._type_ref, typ_ref)
+
+    def test_type_pool(self):
+        typ1 = self.base.Type_([], None, False, None, None)
+        typ2 = self.base.Type_([], None, False, None, None)
+        pool = self.base.Type_Pool_([typ1, typ2])
+        self.assertEqual(pool._types, [typ1, typ2])
+
+    def test_unit_pool(self):
+        value = self.base.Value_('value')
+        typ_ref = self.base.Type_.Reference_(self.base.Value_('alias'))
+        unit = self.base.Unit_(value, typ_ref)
+        pool = self.base.Unit_Pool_([unit])
+        self.assertEqual(pool._units, [unit])
+
+    def test_group(self):
+        type_pool = self.base.Type_Pool_([self.base.Type_([], None, False, None, None)])
+        unit_pool = self.base.Unit_Pool_([self.base.Unit_(self.base.Value_('value'), self.base.Type_.Reference_(self.base.Value_('alias')))])
+        group = self.base.Group_(type_pool, unit_pool)
+        self.assertEqual(group._type_pool, type_pool)
+        self.assertEqual(group._unit_pool, unit_pool)
